b357c4f2aa3a998514da1d0e01a637ccd6a7413b
CNiger <ddmitryi567@gmail.com>
2026-04-04 20:08:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -127,6 +127,3 @@ async def health():
 # Монтируем CAD Cut Service
 app.mount("/cut", cut_app)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
